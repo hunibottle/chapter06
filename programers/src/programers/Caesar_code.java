6346e11066aed18851031080ac146cdb7ca8af86
bang <kimbyeounghoon@naver.com>
2022-01-26 21:42:19 +0900

    " "

--- programers/src/programers/Caesar_code.java
+++ programers/src/programers/Caesar_code.java
@@ -4,17 +4,14 @@ public class Caesar_code {
 	public static void main(String[] args) {
 		
 		String s = "ABCD";
-		int n = 3;
-	
+		int[] arr = new int[s.length()];
 		
 		for(int i = 0 ; i < s.length() ; i++) {
-			String result = s.substring(i,i+1);
-			char ch1 = result.charAt(0);
-			int num = (int)ch1;
-			num = num + n;
-			char ch2 = (char)num;
-			System.out.println(ch2);
+			arr[i] = s.charAt(i) - '0';
+			System.out.println(arr[i]);
 		}
+		 
 		
 	}
+		
 }
