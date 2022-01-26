3d2838e768faeabb4015d50bd4050c35cc830b92
bang <kimbyeounghoon@naver.com>
2022-01-26 21:44:12 +0900

    "score 100 carsar_code"

--- programers/src/programers/Caesar_code.java
+++ programers/src/programers/Caesar_code.java
@@ -4,11 +4,25 @@ public class Caesar_code {
 	public static void main(String[] args) {
 		
 		String s = "ABCD";
-		int[] arr = new int[s.length()];
-		
-		for(int i = 0 ; i < s.length() ; i++) {
-			arr[i] = s.charAt(i) - '0';
-			System.out.println(arr[i]);
+		int n = 3;
+		String answer = "";		
+		for(int i = 0 ; i < s.length(); i++) {
+			char charnum = s.charAt(i);
+			if(charnum >= 'a' && charnum <= 'z') {
+				if(charnum + n > 'z') {
+					answer += (char)(charnum-26+n);
+				}else {
+					answer += (char)(charnum+n);
+				}
+			}
+			else if(charnum >= 'A' && charnum <= 'Z') {
+				if(charnum + n > 'Z') {
+					answer += (char)(charnum-26+n);
+				}else {
+					answer += (char)(charnum+n);
+				}
+			}
+			
 		}
 		 
 		
