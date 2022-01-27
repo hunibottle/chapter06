dadde1597689deb00d54febcb24e706f387427df
bang <kimbyeounghoon@naver.com>
2022-01-28 01:48:34 +0900

    lv2 -가 앞에 붙어 있을때

---
+++ programers/src/programers/maxmin.java
@@ -0,0 +1,16 @@
+package programers;
+
+public class maxmin {
+
+	public static void main(String[] args) {
+		String answer = "-1 -2 3 4";
+		String[] arr = answer.split(""); // answer 문자열에서 공백을 split한다.
+		int[] data = new int[arr.length];
+		
+		int n;
+		data[0] = Integer.parseInt(arr[0]);  
+		System.out.println(data[0]);
+		
+	}
+	
+}
