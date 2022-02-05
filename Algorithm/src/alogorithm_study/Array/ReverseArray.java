c409ebd3d0295b343dcb1b36f23f667d1a203fcd
bang <kimbyeounghoon@naver.com>
2022-02-05 17:46:59 +0900

    ReverseArray

---
+++ Algorithm/src/alogorithm_study/Array/ReverseArray.java
@@ -0,0 +1,28 @@
+package alogorithm_study.Array;
+
+public class ReverseArray {
+
+	
+	public static void Reverse(int[] a) {
+		for(int i = 0 ; i < a.length/2 ; i++) {
+			swap(a, i, a.length-i-1);
+		}
+	}
+	
+	public static void swap(int[] a, int idx1, int idx2) {	
+		int t = a[idx1];
+		a[idx1] = a[idx2];
+		a[idx2] = t;
+	}
+	
+	public static void main(String[] args) {
+		
+		int[] x = {1, 3, 5, 7, 9};
+		
+		Reverse(x);
+		
+		for(int i = 0 ; i < x.length ; i++) {
+			System.out.print(x[i] + " ");
+		}
+	}
+}
