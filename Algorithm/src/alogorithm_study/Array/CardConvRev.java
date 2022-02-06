4a3c5a2a243af36f821794468e930c413424a507
bang <kimbyeounghoon@naver.com>
2022-02-06 14:45:53 +0900

    CardConvRev

---
+++ Algorithm/src/alogorithm_study/Array/CardConvRev.java
@@ -0,0 +1,34 @@
+package alogorithm_study.Array;
+
+public class CardConvRev {
+	
+	static int CardConvR(int x, int r, char[] d) {	
+		int digits = 0;
+		String dchar = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		
+		do {
+			d[digits++] = dchar.charAt(x%r);
+			x /= r;
+		}while(x!=0);
+		
+		return digits;
+	}
+
+	public static void main(String[] args) {
+		
+		
+		
+		int no = 59;
+		int cd = 2;
+		int dno;
+		char[] cno = new char[32];
+		
+		dno = CardConvR(no, cd, cno);
+		
+		for(int i = dno - 1; i >=0 ; i--) {
+			System.out.print(cno[i]);
+		}
+		
+	}
+	
+}
