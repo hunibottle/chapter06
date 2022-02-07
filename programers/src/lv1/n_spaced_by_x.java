62b0250051df6627d61ef6dfa426d8430968b10a
bang <kimbyeounghoon@naver.com>
2022-02-07 10:03:34 +0900

    n_spaced_by_x

--- programers/src/lv1/n_spaced_by_x.java
+++ programers/src/lv1/n_spaced_by_x.java
@@ -5,6 +5,9 @@ public class n_spaced_by_x {
 		int x = -4;
 		int n = 5;
 		
+		long temp = x;
+		//주어진 문제에서 x의 범위가 int의 범위를 초과함으로 temp 매개변수를 통한 정수형 변환이 필요하다.
+		
 		long[] answer = new long[n];
 		
 		for(int i = 0 ; i < 5 ; i++) {
