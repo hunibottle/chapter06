1557dc70fceda9782a866b6c57e3dc78019d85fa
kimbyeounghoon <kimbyeounghoon@naver.com>
2022-03-04 02:13:41 +0900

    하노이탑 다시 풀어보기

---
+++ Algorithm/baekJoon!!/algorithm_study/Recursive/Hanoi.java
@@ -0,0 +1,26 @@
+package algorithm_study.Recursive;
+
+import java.util.Scanner;
+
+public class Hanoi {
+	
+	static void move(int no, int x, int y) {
+		if(no>1) {
+			move(no-1,x,6-x-y);
+		}
+		System.out.println("원반["+no+"]을 " + x + "기둥 에서 " +y+"기둥으로 이동한다.");
+		if(no>1) {
+			move(no-1,6-x-y,y);
+		}
+	}
+	
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.print("원반의 갯수 : ");
+		int no = scan.nextInt();
+		move(no,1,3);
+		
+	}
+
+}
